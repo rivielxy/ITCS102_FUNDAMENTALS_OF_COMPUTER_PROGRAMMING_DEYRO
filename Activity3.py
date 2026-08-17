a3d63c1c6a1d8c7c9bbe0c3f98e1652911757429
Riviel <81639665+rivielxy@users.noreply.github.com>
2026-08-17 11:25:59 +0800

    Activity 3 of the Fundamentals

---
+++ Activity3.py
@@ -0,0 +1,37 @@
+# Act 3: Variables
+
+# Integer
+age = 19
+notebook = 5
+paper = 3
+pen = 10
+
+# Float
+temp = 38.6
+food_weight = 1.3
+weight = 75.8
+height = 173.7
+
+# String
+name = "Dan Gabriel D. Deyro"
+year_section = "2C"
+course = "BSIT"
+subject = "ITCS102"
+
+# Boolean
+is_experienced = True
+is_gamer = True
+is_rich = False
+have_job = False
+
+print("My name is " + name)
+print("I am ", age, " years old.")
+print("My body weight & height is ", weight, "kg and ", height, "cm.") 
+print("My course is " + course + " from section " + year_section + " with a subject " + subject)
+print("The temperature in this classroom is ", temp, " celcius.")
+print("The weight of the food I am holding is ", food_weight, "kg.")
+print("I own ", notebook, " notebooks, ", paper, " papers, and ", pen, " pens.")
+print("Is this user experienced: ", is_experienced)
+print("Is this user a gamer: ", is_gamer)
+print("Is this user rich: ", is_rich)
+print("Is this user have job: ", have_job)
