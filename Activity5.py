5d59e3ac4bbfe394f76d901c741146769a555aa8
Riviel <81639665+rivielxy@users.noreply.github.com>
2026-08-26 10:55:03 +0800

    Activity 5

---
+++ Activity5.py
@@ -0,0 +1,22 @@
+# Arithmetic Operators
+
+a = 8
+b = 6
+c = 0
+
+print("A = ", a)
+print("B = ", b)
+c = a + b
+print("The sum of A and B is ", c) 
+c = a - b
+print("The difference of A and B is ", c) 
+c = a * b
+print("The multiplication of A and B is ", c) 
+c = a / b
+print("The division of A and B is ", c) 
+c = a % b
+print("The modulo of A and B is ", c) 
+c = a ** b
+print("The exponent of A to the power of B is ", c) 
+c = a // b
+print("The floor division of A and B is ", c) 
