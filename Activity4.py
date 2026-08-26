dbdbd026be3cd2fb23ff4675364bd1e9af49bdc2
Riviel <81639665+rivielxy@users.noreply.github.com>
2026-08-26 09:59:25 +0800

    Activity 4 & Code_Challenge 1

---
+++ Activity4.py
@@ -0,0 +1,11 @@
+# Escape Sequence
+# \n
+# \t
+# \r
+# \b
+# \\
+# \"
+
+statement = "\r\t\"Python is one of the most basic programming language. \\\n\tIt is known for itss\b flexibility and stuffs.\""
+
+print(statement)
---
+++ code_challenge1.py
@@ -0,0 +1,10 @@
+# Code Challenge 1 (Pyramid Asterisk)
+
+a = "\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t *\t\t\t\t\t\t*\n"
+b = "\t\t\t\t\t\t\t\t\t**"
+c = "\t\t\t\t\t\t**\n"
+d = "\t\t\t\t\t\t\t\t\t**\t"
+e = "\t**\n"
+f = "\t\t\t\t\t\t\t\t\t***\t*\t*\t**\t*\t*      ***\n"
+
+print(a, b, c, d, "Hi, My name is Dan Gabriel D. Deyro", e, d, "\t\tBSIT-2C\t\t", e, f, f)
