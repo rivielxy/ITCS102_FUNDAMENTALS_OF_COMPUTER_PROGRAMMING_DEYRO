edffbd148841211123bce280ef0660cf82dc6a50
Riviel <81639665+rivielxy@users.noreply.github.com>
2026-09-04 11:02:02 +0800

    Create Activity7.py

---
+++ Activity7.py
@@ -0,0 +1,53 @@
+# ASSIGNMENT OPERATOR
+
+result = 10
+print("Your current number is: ", result)
+a = eval(input("Enter the number you want to add: ---> "))
+result += a
+
+print("Your partial result is: ", result)
+
+b = eval(input("Enter the number you want to add: ---> "))
+result += b
+
+print("Your partial result is: ", result)
+
+c = eval(input("Enter the number you want to subtract: ---> "))
+result -= c
+
+print("Your partial result is: ", result)
+
+d = eval(input("Enter the number you want to subtract: ---> "))
+result -= d
+
+print("Your partial result is: ", result)
+
+e = eval(input("Enter the number you want to add: ---> "))
+result += e
+
+print("Your partial result is: ", result)
+
+f = eval(input("Enter the number you want to subtract: ---> "))
+result -= f
+
+print("Your partial result is: ", result)
+
+g = eval(input("Enter the number you want to multiply: ---> "))
+result *= g
+
+print("Your partial result is: ", result)
+
+h = eval(input("Enter the number you want to divide: ---> "))
+result /= h
+
+print("Your partial result is: ", result)
+
+i = eval(input("Enter the number you want to add: ---> "))
+result += i
+
+print("Your partial result is: ", result)
+
+j = eval(input("Enter the number you want to add: ---> "))
+result += j
+
+print("Your total result is: ", result)
