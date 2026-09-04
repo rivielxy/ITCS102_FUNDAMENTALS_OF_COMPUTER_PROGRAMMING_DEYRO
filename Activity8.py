afe4f26ce36a1a84f220ed173a135d93c05106fb
Riviel <81639665+rivielxy@users.noreply.github.com>
2026-09-04 11:16:22 +0800

    Update Activity8.py

--- Activity8.py
+++ Activity8.py
@@ -9,6 +9,27 @@ b = input("What else? ")
 hobbies += b + ", "
 
 c = input("Anything else? ")
-hobbies += c + "."
+hobbies += c + ", "
+
+d = input("Anything else? ")
+hobbies += d + ", "
+
+e = input("Anything else? ")
+hobbies += e + ", "
+
+f = input("Anything else? ")
+hobbies += f + ", "
+
+g = input("Anything else? ")
+hobbies += g + ", "
+
+h = input("Anything else? ")
+hobbies += h + ", "
+
+i = input("Anything else? ")
+hobbies += i + ", "
+
+j = input("Anything else? ")
+hobbies += j + "."
 
 print("Your hobbies are: {", hobbies, "}")
